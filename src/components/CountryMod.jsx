87cdb923b5c1d807ec7738a88c29d0f48c675c98
rakib hasan <rakibwebdev78@gmail.com>
2025-02-06 04:01:15 +0600

    data adding started...

--- src/components/CountryMod.jsx
+++ src/components/CountryMod.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons/faX';
 import Text from './../utilities/Text';
+import Title from './../utilities/Title';
+import Countries from './Countries';
 
 const CountryMod = ({country, onClick}) => {
     console.log(country);
@@ -30,7 +32,7 @@ const CountryMod = ({country, onClick}) => {
     } = country;
 
     return (
-        <div className='modal-glass'>
+        <div className='modal-glass z-40'>
             {/* closing modal---> */}
             <div className='w-full flex items-center justify-end'>
 
@@ -47,7 +49,24 @@ const CountryMod = ({country, onClick}) => {
             </div>
             <div className='basic-modal-body xxm:flex-col-reverse lg:flex-row xxm:space-y-10 lg:space-x-10 lg:space-y-0'>
             {/* info section */}
-            <div className='w-8/12 border'></div>
+            <div className='w-8/12'>
+                <div className='w-full flex items-center justify-start'>
+                    <Title tag={3} text={`country : ${common}`} className={`text-white/70`} />
+                </div>
+                <div className='w-full flex flex-col items-start justify-start'>
+                    <Title tag={3} text={`official name: ${official}`} className={`text-white/70`} />
+                    <Text tag={`p`} text={`area : ${area} SqKm.`}/>
+                    <Text tag={`p`} text={`${continents.length>1 ? 'continents':'continent'} : ${continents.join(', ')}`} />
+                    <Text tag={`p`} text={`region : ${region}`}/>
+                    <Text tag={`p`} text={`subregion: ${subregion ||'not mentioned.'}`}/>
+                    <Text tag={`p`} text={`${borders.length<2 ? 'neighbor country ': 'neighbor Countries'} : ${borders.join(' , ') || 'lone island or islands.'} `}/>
+                    <Text tag={`p`} text={`independent status : ${independent?'independent country':'not independent country'}`} />
+                    <Text tag={`p`} />
+                    <Text tag={`p`} />
+                    <Text tag={`p`} />
+                    <Text tag={`p`} />
+                </div>
+            </div>
             {/* flag section */}
             <div className='w-3/12 border'></div>
             </div>
